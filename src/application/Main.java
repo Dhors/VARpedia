9ea8ca132463ffa2288703b6bc9c191f65a3e98e
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2019-09-23 14:12:01 +1200

    Update Main.java
    added main() method

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
     @Override
     public void start(Stage stage) throws Exception {
