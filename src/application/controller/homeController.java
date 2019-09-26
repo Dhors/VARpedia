77c9e9ec792a5802ffa27d3129ae34c66633f1f9
Dhors <smexylure@gmail.com>
2019-09-26 15:05:00 +1200

    testing commits

--- src/application/controller/homeController.java
+++ src/application/controller/homeController.java
@@ -1,5 +1,40 @@
 package application.controller;
 
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class homeController {
+
+        @FXML
+        private void handleCreationButton() throws IOException {
+            titleText.setText("the change to commit");
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("resources/CreationStage.fxml"));
+            Parent layout = loader.load();
+            Scene scene = new Scene(layout);
+            Stage creationStage = new Stage();
+            creationStage.setScene(scene);
+            creationStage.show();
+
+           // Parent CreationViewParent = FXMLLoader.load(getClass().getResource("resources/CreationStage.fxml"));
+           // Scene CreationViewScene = new Scene(CreationViewParent )
+
+
+
+        }
+
+
+
+        @FXML
+        private Text titleText;
 
 }
