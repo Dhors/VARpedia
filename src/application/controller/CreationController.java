da03ab31a08a57c11eaf2ae1dbbbad924b9203a4
RawiriHohepa <RawiriHohepa@gmail.com>
2019-10-09 14:43:41 +1300

    search button disabled until search term entered

--- src/application/controller/CreationController.java
+++ src/application/controller/CreationController.java
@@ -2,13 +2,21 @@ package application.controller;
 
 import application.BashCommand;
 import application.Main;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import application.ImageVideoTask;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -21,6 +29,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -86,11 +95,26 @@ public class CreationController {
 	@FXML
 	private Text creationNamePrompt;
 
+	@FXML
+	private void initialize() {
+		searchWikipediaButton.setDisable(true);
+		
+		//========================== ADD REFERENCE, COPY-PASTED CODE =====================================================
+		BooleanBinding textIsEmpty = Bindings.createBooleanBinding(() -> enterSearchTermTextInput.getText().trim().isEmpty(), enterSearchTermTextInput.textProperty());
+		searchWikipediaButton.disableProperty().bind(textIsEmpty);
+		//==========================
+	}
 
 	@FXML
 	private void handleCreationCancelButton(ActionEvent event) throws IOException {
 		// Return to main menu
-		Main.changeScene("resources/listCreationsScene.fxml", event);
+		Parent creationViewParent = FXMLLoader.load(Main.class.getResource("resources/listCreationsScene.fxml"));
+		Scene creationViewScene = new Scene(creationViewParent);
+
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+		window.setScene(creationViewScene);
+		window.show();
 
 		// Cleaning the chunks folder if the creation is cancelled.
 		File folderChunk = new File(System.getProperty("user.dir") + "/chunks/" );
@@ -105,48 +129,45 @@ public class CreationController {
 
 	@FXML
 	private void handleSearchWikipedia(ActionEvent event) throws IOException {
-		_searchTerm = enterSearchTermTextInput.getText();
+		_searchTerm = enterSearchTermTextInput.getText().trim();
 
-		if (_searchTerm.equals("") || _searchTerm.equals(null)) {
-			termNotFound.setVisible(true);
-		} else {
-			termNotFound.setVisible(false);
-			searchInProgress.setVisible(true);
+		termNotFound.setVisible(false);
+		searchInProgress.setVisible(true);
 
-			// Run bash script that uses wikit and returns the the result of the search
-			String[] command = new String[]{"/bin/bash", "-c", "./script.sh search " + _searchTerm};
-			BashCommand bashCommand = new BashCommand(command);
-			team.submit(bashCommand);
+		// Run bash script that uses wikit and returns the the result of the search
+		String[] command = new String[]{"/bin/bash", "-c", "./script.sh search " + _searchTerm};
+		BashCommand bashCommand = new BashCommand(command);
+		team.submit(bashCommand);
 
-			// Using concurrency allows the user to cancel the creation if the search takes too long
-			bashCommand.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-				@Override
-				public void handle(WorkerStateEvent event) {
-					try {
-						/**
-						 * Returns a list with only one element
-						 * If the term was not found, the element is "(Term not found)"
-						 * Otherwise the element is the search result
-						 */
-						List<String> result = bashCommand.get();
-						String searchResult = result.get(0);
+		// Using concurrency allows the user to cancel the creation if the search takes too long
+		bashCommand.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(WorkerStateEvent event) {
+				try {
+					/**
+					 * Returns a list with only one element
+					 * If the term was not found, the element is "(Term not found)"
+					 * Otherwise the element is the search result
+					 */
+					List<String> result = bashCommand.get();
+					String searchResult = result.get(0);
 
-						if (searchResult.equals("(Term not found)")) {
-							searchInProgress.setVisible(false);
-							termNotFound.setVisible(true);
-						} else {
-							searchResultTextArea.setText(searchResult);
-							displayChunkSelection();
-						}
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} catch (ExecutionException e) {
-						e.printStackTrace();
+					if (searchResult.equals("(Term not found)")) {
+						searchInProgress.setVisible(false);
+						termNotFound.setVisible(true);
+					} else {
+						searchResultTextArea.setText(searchResult);
+						displayChunkSelection();
 					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
 				}
-			});
-		}
+			}
+		});
 	}
+
 
 
 	@FXML
@@ -363,7 +384,11 @@ public class CreationController {
 				combineAudioChunks(creationName);
 
 				// return to main menu
-				Main.changeScene("resources/listCreationsScene.fxml", event);
+				Parent creationViewParent = FXMLLoader.load(Main.class.getResource("resources/listCreationsScene.fxml"));
+				Scene creationViewScene = new Scene(creationViewParent);
+				Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+				window.setScene(creationViewScene);
+				window.show();
 
 				Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
 				alert2.setTitle("Creation in progress");
@@ -381,7 +406,11 @@ public class CreationController {
 			}
 			combineAudioChunks(creationName);
 			// return to main menu
-			Main.changeScene("resources/listCreationsScene.fxml", event);
+			Parent creationViewParent = FXMLLoader.load(Main.class.getResource("resources/listCreationsScene.fxml"));
+			Scene creationViewScene = new Scene(creationViewParent);
+			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+			window.setScene(creationViewScene);
+			window.show();
 
 			alertLocal = new Alert(Alert.AlertType.INFORMATION);
 			alertLocal.setTitle("Creation in progress");
